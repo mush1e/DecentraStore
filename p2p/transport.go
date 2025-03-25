9068cf93b5bea2b4beb567bf18e48de525e983a1
mush1e <mustafa.a.r.siddiqui@outlook.com>
2025-03-25 01:11:27 -0400

    [task] restart

--- p2p/transport.go
+++
@@ -1,9 +0,0 @@
-package p2p
-
-// Peer is an ionterface that represents a remote node
-type Peer interface{}
-
-// transport represents anything that handles the
-// communication between nodes in the network. This can be
-// of the form (TCP, UDP, Websockets)
-type Transport interface{}
